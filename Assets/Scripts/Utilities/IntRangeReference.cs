4b2ccd0d71217a4faa2fe1b99febd147754a308d
Michael Pan <panmpan@gmail.com>
2022-04-19 11:42:31 +0800

    Improve value reference editor

--- Assets/Scripts/Utilities/IntRangeReference.cs
+++ Assets/Scripts/Utilities/IntRangeReference.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 [CreateAssetMenu]
 public class IntRangeReference : ScriptableObject
@@ -14,3 +18,43 @@ public class IntRangeReference : ScriptableObject
         return maxInclusive ? Random.Range(Min, Max + 1) : Random.Range(Min, Max);
     }
 }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(IntRangeReference))]
+public class IntRangeReferenceDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 45;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position.y += 1;
+        position.height = 18;
+        EditorGUI.PropertyField(position, property, label);
+
+        Object reference = property.objectReferenceValue;
+        if (reference)
+        {
+            SerializedObject serializedObject = new SerializedObject(reference);
+            serializedObject.Update();
+
+            float originWidth = position.width;
+            position.x += originWidth * 0.4f;
+            position.y += 20;
+
+            position.width = originWidth * 0.3f - 5;
+            SerializedProperty min = serializedObject.FindProperty("Min");
+            min.intValue = EditorGUI.IntField(position, min.intValue);
+
+            position.x += position.width + 10;
+            SerializedProperty max = serializedObject.FindProperty("Max");
+            max.intValue = EditorGUI.IntField(position, max.intValue);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
